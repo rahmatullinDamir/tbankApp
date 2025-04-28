5c9c7b3f7b9ccd6c60519188f73e4e672acdf80d
Damir Rakhmatullin <damirrakhmatuliin@yandex.ru>
2025-04-28 19:14:09 +0300

    ini

--- travelerApp/SceneDelegate.swift
+++ travelerApp/SceneDelegate.swift
@@ -14,11 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let loginViewModel = LoginViewModel()
-        // главный координатор 
+
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: LoginViewController(viewModel: loginViewModel))
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         window?.makeKeyAndVisible()
     }
 
