3b0762532b3acf6a11a6a734c8aebaea409d0f6f
Damir Rakhmatullin <damirrakhmatuliin@yandex.ru>
2025-04-28 18:31:18 +0300

    first trying

---
+++ travelerAppTests/TravelerAppTests.swift
@@ -0,0 +1,15 @@
+//
+//  travelerAppTests.swift
+//  travelerAppTests
+//
+//  Created by Damir Rakhmatullin on 21.04.25.
+//
+
+import Testing
+@testable import travelerApp
+
+struct TravelerAppTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
